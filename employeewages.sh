3a99f0c230f285da7722461a6287a13f76348034
murali1125 <murali1125s@gmail.com>
2020-03-21 23:50:36 +0530

    welcome text in master branch

---
+++ employeewages.sh
@@ -0,0 +1,7 @@
+#! /bin/bash -x
+
+#Start with Displaying welcome to Employee Wage Computation Program on Master Branch
+
+#-----------------------------------------------------------------------------------
+
+echo "   ------------ Welcome to Employee Wage Computation Program ---------------    "
