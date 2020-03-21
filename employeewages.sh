82ac6876f7cd4e8697536f1077ddc3b37aa9cce1
murali1125 <murali1125s@gmail.com>
2020-03-22 00:05:31 +0530

    uc3

--- employeewages.sh
+++ employeewages.sh
@@ -3,18 +3,24 @@
 # Start with Displaying welcome to Employee Wage Computation Program on Master Branch
 # Check Employee is Present or Absent
 # Calculate Daily Employee Wage assume per hour wage =20 daily working hours 8
+#Add Part time Employee & Wage - Assume Part time Hour is 4
 #-----------------------------------------------------------------------------------
 
 echo "   ------------ Welcome to Employee Wage Computation Program ---------------    "
-# randomly select attedance of the employee 1-present 0-absent
-attendance=$(( RANDOM % 2 ))
+# randomly select attedance of the employee 2-parttime present 1-ful time present 0-absent
+attendance=$(( RANDOM % 3 ))
 # wage variable to store total wage
 total_wage=0
 wage=20
 fulltime=8
+parttime=4
 # if employee present calculate wage
 if [[ attendance -eq 1 ]]
 then
 	# total wage = wage per hour * total working hours
 	total_wage=$(( wage * fulltime ))
+elif [[  attendance -eq 2 ]]
+then
+	 total_wage=$(( wage * parttime ))
+
 fi
