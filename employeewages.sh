edd5c02db6026a943a3e04aeb3821938093410f2
murali1125 <murali1125s@gmail.com>
2020-03-22 00:15:15 +0530

    uc5

--- employeewages.sh
+++ employeewages.sh
@@ -5,6 +5,7 @@
 # Calculate Daily Employee Wage assume per hour wage =20 daily working hours 8
 #Add Part time Employee & Wage - Assume Part time Hour is 4
 #Solving using Case Statement
+#Calculating Wages for a Month  Assume 20 Working Day per Month
 #-----------------------------------------------------------------------------------
 
 echo "   ------------ Welcome to Employee Wage Computation Program ---------------    "
@@ -15,11 +16,19 @@ total_wage=0
 wage=20
 fulltime=8
 parttime=4
-# if employee present calculate wage
-case attedance in 
-	1)
-	# total wage = wage per hour * total working hours
-		total_wage=$(( wage * fulltime )) ;;
-	2) #partime wage calculation
-		total_wage=$(( wage * parttime )) ;;
-esac
+# runthe loop 20 times to caluculate 20days wage
+count=1
+while [[ $count -le 20 ]]
+do
+	# if employee present calculate wage
+	case attedance in 
+		1)
+		# total wage = wage per hour * total working hours
+			total_wage=$(( wage * fulltime )) ;;
+		2) #partime wage calculation
+			total_wage=$(( wage * parttime )) ;;
+	esac
+	(( count++ ))
+done
+
+echo " wage of the employee  =  $total_wage "
