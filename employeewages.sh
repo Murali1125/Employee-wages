e6a327a1a756268e208a6cb86476a645f3e49105
murali1125 <murali1125s@gmail.com>
2020-03-22 00:44:23 +0530

    uc8

--- employeewages.sh
+++ employeewages.sh
@@ -7,6 +7,8 @@
 #Solving using Case Statement
 #Calculating Wages for a Month  Assume 20 Working Day per Month
 #Calculate Wages till a condition of total working hours or days is reached for a month - Assume 100 hours
+#Refactor the Code to write a function to get work hours
+#Store the Daily Wage along with the Total Wage
 #---------------------------------------------------------------------------------------------------------
 
 echo "   ------------ Welcome to Employee Wage Computation Program ---------------    "
@@ -21,6 +23,8 @@ parttime=4
 count=1
 # hours variable
 hours=0
+# declare a dictionary to store daily wages
+declare -A wage_dic
 
 function work_hours () {
 	if  [[ $count -gt 20 -o $hours -ge 100 ]]
@@ -37,15 +41,18 @@ function work_hours () {
 			then
 				hours=100
 
-			fi ;;
+			fi
+			wage_dic[$count]=$(( 20 * 8 )) ;;
 		2)  #if it part increase hours + 4
 			hours=$(( hours + 4 )) 
 			 if [[ $hours -gt 100 ]]
                         then
                                 hours=100
 
-                        fi ;;
+                        fi 
+			wage_dic[$count]=$(( 20 * 4 ));;
 		0) # absent then increase the day only no hours
+			wage_dic[$count]=0
 	esac
 	(( count++ ))
 	work_hours
@@ -54,3 +61,7 @@ function work_hours () {
 # total wage = total hours * 20
 total_wage=$(( hours * 20 ))
 echo " wage of the employee  =  $total_wage "
+for (( i=1;i<=${#wage_dic};i++ ))
+do
+	echo " wage of day$i  = ${wage_dic[$i]} "
+done
