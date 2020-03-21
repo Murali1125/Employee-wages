25af50566f5ec28cba2424d4f48ed105f72f4726
murali1125 <murali1125s@gmail.com>
2020-03-21 23:54:14 +0530

    uc1

--- employeewages.sh
+++ employeewages.sh
@@ -1,7 +1,9 @@
 #! /bin/bash -x
 
-#Start with Displaying welcome to Employee Wage Computation Program on Master Branch
-
+# Start with Displaying welcome to Employee Wage Computation Program on Master Branch
+# Check Employee is Present or Absent
 #-----------------------------------------------------------------------------------
 
 echo "   ------------ Welcome to Employee Wage Computation Program ---------------    "
+# randomly select attedance of the employee 1-present 0-absent
+attendance=$(( RANDOM % 2 ))
